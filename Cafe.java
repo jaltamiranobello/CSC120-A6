f5051d7477de41f1fd2b8f49f7005e73343aa311
jaltamiranobello <jaltamiranobello@smith.edu>
2023-03-26 20:56:53 -0400

    done with all classes without headers

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,63 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building {
+    private int nCoffeeOunces;
+    private int nSugarPackets;
+    private int nCreams; 
+    private int nCups;
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+    public Cafe(String name, String address, int nFloors, int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
+    }
+
+    public int getnCoffeeOunces(){
+        return this.nCoffeeOunces;
+    }
+
+    public int getnSugarPackets(){
+        return this.nSugarPackets;
+    }
+
+    public int getnCreams(){
+        return this.nCreams;
+    }
+
+    public int getnCups(){
+        return this.nCups;
     }
     
+    public void sellCoffee(int size, int nSugarPackets, int nCreams){
+        try {
+            if (this.nCoffeeOunces <= size - 1 | this.nSugarPackets <= nSugarPackets - 1 | this.nCreams <= nCreams - 1 | this.nCups == 0){
+                throw new RuntimeException("Sorry we don't have enough inventory to make the coffee. Please give us a moment and try again!");
+            }
+
+            this.nCoffeeOunces -= size;
+            this.nSugarPackets -= nSugarPackets;
+            this.nCreams -= nCreams;
+            this.nCups -= 1;
+            System.out.println("Thank you for your purchase!");
+        }
+         catch(RuntimeException e){
+            System.out.println(e);
+            this.restock(size, nSugarPackets, nCreams, 1);
+         }
+    }
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
+    }
     public static void main(String[] args) {
-        new Cafe();
+        Cafe myCafe = new Cafe("Compass Cafe", "Neilson Library", 1, 12, 4, 4, 2);
+        myCafe.sellCoffee(12, 2, 2);
+        myCafe.sellCoffee(16, 4, 4);
+        myCafe.sellCoffee(12, 2, 2);
     }
     
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,79 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
+/* This is a stub for the Library class */
+public class Library extends Building {
+    private Hashtable <String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable <String, Boolean> ();
     }
-  
+
+    /** Accessor for collection */
+    public Hashtable <String, Boolean> getCollection(){
+      return this.collection;
+    }
+
+
+    public void addTitle(String title){
+      this.collection.put(title, true);
+    }
+
+    // return the title that we removed
+    public String removeTitle(String title){
+      this.collection.remove(title);
+      return title;
+    }
+
+    public void checkOut(String title){
+      this.collection.replace(title, false);
+    }
+
+    public void returnBook(String title){
+      this.collection.replace(title, true);
+    }
+
+    // returns true if the title appears as a key in the Libary's collection, false otherwise
+    public boolean containsTitle(String title){
+      if (this.collection.containsKey(title)){
+        return true;
+      }
+      else{
+        return false;
+      }
+      
+    }
+    
+    // returns true if the title is currently available, false otherwise
+    public boolean isAvailable(String title){
+      if(this.collection.get(title).equals(true)){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+    public String toString() {
+      String description = super.toString();
+      description += ". There are currently " + this.collection.size() + " books in this collection.";
+      return description;
+    }
+
+    // prints out the entire collection in an easy-to-read way (including checkout status)
+    public void printCollection() {
+      System.out.println(this.collection.toString());
+    } 
+
+
+
     public static void main(String[] args) {
-      new Library();
+      Library neilson = new Library("Neilson", "7 Smith St", 4);
+      neilson.addTitle("The Cat in The Hat By Dr. Seuss");
+      System.out.println(neilson);
+      neilson.printCollection();
+
+
     }
   
   }
